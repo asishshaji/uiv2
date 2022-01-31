4209ce632ce46191fb47bf88e54dd771ce539f5f
asishshaji <asishshajithomas@gmail.com>
2022-01-31 10:51:49 +0530

    Web build

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:uiv2/screens/dashboard.dart';
-import 'package:window_manager/window_manager.dart';
+// import 'package:window_manager/window_manager.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await windowManager.ensureInitialized();
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await windowManager.ensureInitialized();
 
-  windowManager.waitUntilReadyToShow().then((_) async {
-    windowManager.show();
-    await windowManager.setFullScreen(true);
-  });
+  // windowManager.waitUntilReadyToShow().then((_) async {
+  //   windowManager.show();
+  //   await windowManager.setFullScreen(true);
+  // });
   runApp(
     const ProviderScope(
       child: MaterialApp(
